053cf17c89d14f45911274b27657b4c37d444e42
jordanhuaman <jordanelcracken@gmail.com>
2025-06-03 18:11:40 -0500

    feat: simple backend and hono condiguration

--- v1/src/index.ts
+++ v1/src/index.ts
@@ -1,9 +1,24 @@
+import "reflect-metadata"
 import { Hono } from 'hono'
+import { AppDataSource } from "./moock/rest/infraestructure/postgres/globalconfig"
+import { ApolloServer } from "@apollo/server"
+import { resolvers, typeDefs } from "./moock/shared/schemas/todo.schema"
+import { startStandaloneServer } from "@apollo/server/dist/esm/standalone"
+
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+const graphqlServer = new ApolloServer({
+  typeDefs: typeDefs,
+  resolvers: resolvers
 })
+
+
+AppDataSource.initialize()
+  .then(async () => {
+    app.get('/', (c) => {
+      return c.json({ msg: "hid" })
+    })
+  })
 
 export default app

038942ba1f4c7d0db845a653c25eb6bcd2810441
jordanhuaman <jordanelcracken@gmail.com>
2025-12-27 08:26:50 -0500

    feat: commit collback

--- todo-service/src/mook/user/infraestructure/user-controller.ts
+++ todo-service/src/mook/user/infraestructure/user-controller.ts
@@ -1,5 +1,5 @@
 import type { userRepository } from "./user-repository";
-
+import { tryCatch as customTryCatch } from "../../../shared/domain/custm_try-catch";
 export class UserController {
   constructor(readonly userRepository: userRepository) { }
 
@@ -7,7 +7,11 @@ export class UserController {
     return this.userRepository.getallUsers();
   }
   async createUser(data: { name: string; age: number; email: string }): Promise<string> {
-    const persisted = await this.userRepository.createUser(data.name, data.age, data.email);
-    return persisted
+    const {data: result , error} = await customTryCatch(this.userRepository.createUser(data.name, data.age, data.email));
+
+    if (error){
+      throw new Error(`Error creating user`);
+    }
+    return result;
   }
 }
